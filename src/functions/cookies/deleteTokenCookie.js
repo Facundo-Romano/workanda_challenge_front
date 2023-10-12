010efce938fc25cfd3f9129fd4e49e819d995c0b
Facundo-Romano <facuromano121299@gmail.com>
2023-10-12 18:41:32 -0300

    logout

---
+++ src/functions/cookies/deleteTokenCookie.js
@@ -0,0 +1,5 @@
+function deleteTokenCookie() {
+    document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+};
+
+export default deleteTokenCookie;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="home">
     <h1>HOME</h1>
+    <button @click="logout(email, password)">Logout</button>
   </div>
 </template>
 
-<style>
-</style>
+<script setup>
+  import deleteTokenCookie from '@/functions/cookies/deleteTokenCookie.js';
+  import router from '@/router/index.js';
+</script>
+
+<script>
+  function logout() {
+    try {
+      deleteTokenCookie();
+
+      router.push('/login');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+</script>
