762df7484de2c0a835d7874fb7b61e131fc90347
Facundo-Romano <facuromano121299@gmail.com>
2023-10-12 18:58:03 -0300

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,42 @@
   <div class="home">
     <h1>HOME</h1>
     <button @click="logout(email, password)">Logout</button>
+    <li v-for="user in users">
+      {{ user.email }}
+    </li>
   </div>
 </template>
 
 <script setup>
+  import customAxios from '@/functions/axios/customAxios.js';
   import deleteTokenCookie from '@/functions/cookies/deleteTokenCookie.js';
   import router from '@/router/index.js';
 </script>
 
 <script>
-  function logout() {
-    try {
-      deleteTokenCookie();
+  export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    mounted() {
+      this.getUsers();
+    },
+    methods: {
+      logout() {
+        deleteTokenCookie();
+        router.push('/login');
+      },
+      async getUsers() {
+        try {
+          const response = await customAxios.get('/users/getAll');
 
-      router.push('/login');
-    } catch (err) {
-      console.log(err);
-    }
-  }
+          this.users = response.data.users;
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    },
+  };
 </script>
