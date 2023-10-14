c461e7db3c2888e4d52ce6e579c31a9ccf393993
Facundo-Romano <facuromano121299@gmail.com>
2023-10-14 16:43:47 -0300

    same site cookie configuration

--- src/functions/cookies/setTokenCookie.js
+++ src/functions/cookies/setTokenCookie.js
@@ -2,7 +2,7 @@ function setTokenCookie(value) {
     const date = new Date();
     date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
-    document.cookie = "jwt=" + value + ";" + expires + ";path=/";
+    document.cookie = "jwt=" + value + ";" + expires + ";path=/; Secure; SameSite=None";
 };
 
 export default setTokenCookie;
